62fc8d7b1d9088128072b586f80cfbaf7d74dca2
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-10-24 22:05:09 +0530

    Added Environment Variable Port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express(); //by convection this func returns an object called app
 // making a http get request at the end point '/'
 app.get("/", (req, res) => {
   // callback function called a route handler
-  res.send("Hello World");
+  res.send("Hello World!!");
 });
 // app.post();
 // app.put();
@@ -16,6 +16,13 @@ app.get("/api/courses", (req, res) => {
   //query a database irl
   res.send([1, 2, 3]);
 });
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+
+//PORT
+// port is dyanmically assigned by the hosting environment & we can't rely on 3000 to be available
+// PORT is an an environment variable(is the part of environment on which the process runs).
+// process is global object
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
