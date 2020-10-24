774b17fb22f3fe9089ada48fcb1c0899a12165d7
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-10-24 22:44:21 +0530

    Handling http post requests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express(); //by convection this func returns an object called app
 
+// middleware
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -14,12 +17,23 @@ app.get("/", (req, res) => {
   // callback function called a route handler
   res.send("Hello World!!");
 });
-// app.post();
+
 // app.put();
 // app.delete();
 app.get("/api/courses", (req, res) => {
   //query a database irl
   res.send(courses);
+});
+
+//http post request to create a new course
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  // client always needs to know about the properties of the course
+  res.send(course);
 });
 
 // api/courses/1
