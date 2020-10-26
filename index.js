f90b17a01e9b0b93d08fe6c9270ce90174206320
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-10-26 08:43:24 +0530

    Changes Made

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const debug = require("debug")("app:startup");
 const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const Joi = require("joi");
-const logger = require("./logger");
-const authenticate = require("./authentication");
+const logger = require("./middleware/logger");
+const authenticate = require("./middleware/authentication");
+const courses = require("./routes/courses");
+const home = require("./routes/home");
 const express = require("express");
 const app = express(); //by convection this func returns an object called app
 
@@ -15,11 +16,12 @@ app.use(express.json()); //parse the body of request into a json object by setti
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); //served from the root
 app.use(helmet());
+app.use("/api/courses", courses);
+app.use("/", home);
 
 // Config
 console.log("Application Name: " + config.get("name"));
 console.log("Mail Server: " + config.get("mail.host"));
-
 console.log("Mail Password: " + config.get("mail.password"));
 
 if (app.get("env") === "development") {
@@ -29,105 +31,6 @@ if (app.get("env") === "development") {
 
 app.use(logger);
 app.use(authenticate);
-
-const courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
-// app object has methods corresponding to the http methods
-
-// making a http get request at the end point '/'
-app.get("/", (req, res) => {
-  // callback function called a route handler
-  res.render("index", { title: "My Express App", message: "Hello" });
-});
-
-// app.put();
-// app.delete();
-app.get("/api/courses", (req, res) => {
-  //query a database irl
-  res.send(courses);
-});
-
-//http post request to create a new course
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) {
-    // 400 bad request
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const course = {
-    id: courses.length + 1,
-    //validate what client sends you
-    name: req.body.name,
-  };
-  courses.push(course);
-  // client always needs to know about the properties of the course
-  res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  // find course
-  // if not existing return 404
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    // 404
-    return res
-      .status(404)
-      .send(`The course with the given ID ${req.params.id} was not found`);
-  }
-
-  //validate
-  // if invalid return 400 - Bad request
-  const { error } = validateCourse(req.body);
-  if (error) {
-    // 400 bad request
-    return res.status(400).send(error.details[0].message);
-  }
-  //Update course
-  // Return the updated course
-  course.name = req.body.name;
-  res.send(course);
-});
-
-// api/courses/1
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    // 404
-    res
-      .status(404)
-      .send(`The course with the given ID ${req.params.id} was not found`);
-  } else {
-    res.send(course);
-  }
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  //Look up
-  // Not existing, return 404
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    // 404
-    return res
-      .status(404)
-      .send(`The course with the given ID ${req.params.id} was not found`);
-  }
-  //Delete
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  //Return the same course
-  res.send(courses);
-});
-
-const validateCourse = (course) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  return schema.validate(course);
-};
 
 //PORT
 // port is dyanmically assigned by the hosting environment & we can't rely on 3000 to be available
